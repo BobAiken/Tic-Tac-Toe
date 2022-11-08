6af1dd3d855fd9ac0c02b5a663f51cc1d2e21ae7
Robert Aiken <robertsiraaiken@gmail.com>
2022-11-08 11:58:32 -0600

    add framework for classes

---
+++ game.js
@@ -0,0 +1,17 @@
+class Game{
+    constructor(player1,player2){
+        this.gameBoard = []
+        this.turn = false
+        this.player1 = player1
+        this.player2 = player2
+    }
+    checkForWin(){
+
+    }
+    checkForDraw(){
+
+    }
+    resetGame(){
+        
+    }
+}
